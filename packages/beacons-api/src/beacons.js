fa2f37ee096362f981626bacf7fd7f49430b557b
Peter Moser <p.moser@noi.bz.it>
2019-07-11 16:36:46 +0200

    Remove allorigins proxy

--- packages/beacons-api/src/beacons.js
+++ packages/beacons-api/src/beacons.js
@@ -1,13 +1,12 @@
 export async function getBeacons() {
-  let response = await fetch('https://api.allorigins.win/get?url=' + 'https://api.beacon.bz.it/v1/infos', {
+  let response = await fetch('https://api.beacon.bz.it/v1/infos', {
     method: 'GET',
     headers: new Headers({
       'Accept': 'application/json'
     })
   })
 
-  let proxyResponse = await response.json()
-  let json = JSON.parse(proxyResponse.contents)
+  let json = await response.json()
 
   return json.filter(beacon => !!beacon.latitude && !!beacon.longitude && (!!beacon.address || !!beacon.location))
 }
