8d281265d5cb087dee9b00c50b30e892f0a26da5
Daniel Rampanelli <hello@danielrampanelli.com>
2019-07-11 16:30:03 +0200

    Add support for sorting the most relevant table columns.

--- packages/beacons-table/src/table.js
+++ packages/beacons-table/src/table.js
@@ -1,14 +1,16 @@
 import { LitElement, html } from 'lit-element'
+import _ from 'lodash'
 import '@polymer/paper-spinner/paper-spinner.js'
 import '@vaadin/vaadin-grid/vaadin-grid.js'
+import '@vaadin/vaadin-grid/vaadin-grid-sort-column.js'
 
 class BeaconsTableView extends LitElement {
 
-  constructor () {
+  constructor() {
     super()
   }
 
-  render () {
+  render() {
     return html`
       <style>
         :host {
@@ -21,32 +23,32 @@ class BeaconsTableView extends LitElement {
           width: 100%;
         }
       </style>
-      <vaadin-grid id="table" theme="row-dividers">
+      <vaadin-grid id="table" theme="row-dividers" multi-sort>
         <vaadin-grid-column id="idcolumn" header="UUID" path="uuid"></vaadin-grid-column>
-        <vaadin-grid-column id="majorcolumn" header="MAJOR" path="major"></vaadin-grid-column>
-        <vaadin-grid-column id="minorcolumn" header="MINOR" path="minor"></vaadin-grid-column>
+        <vaadin-grid-sort-column id="majorcolumn" header="MAJOR" path="major"></vaadin-grid-sort-column>
+        <vaadin-grid-sort-column id="minorcolumn" header="MINOR" path="minor"></vaadin-grid-sort-column>
         <vaadin-grid-column id="latitudecolumn" header="LATITUDE" path="latitude"></vaadin-grid-column>
         <vaadin-grid-column id="longitudecolumn" header="LONGITUDE" path="longitude"></vaadin-grid-column>
-        <vaadin-grid-column id="locationcolumn" header="LOCATION"></vaadin-grid-column>
+        <vaadin-grid-sort-column id="locationcolumn" header="LOCATION" path="composedLocation"></vaadin-grid-sort-column>
       </vaadin-grid>
     `
   }
 
-  bind (beacons, filter) {
+  bind(beacons, filter) {
     let self = this
 
-    self.table.items = beacons
+    self.table.items = beacons.map((beacon) => {
+      return _.extend({
+        composedLocation: (beacon.location || beacon.address) + ' (' + beacon.cap + ')'
+      }, _.cloneDeep(beacon))
+    })
   }
 
-  async firstUpdated () {
+  async firstUpdated() {
     let self = this
     let root = this.shadowRoot
 
     self.table = root.getElementById('table')
-
-    root.getElementById('locationcolumn').renderer = (root, grid, rowData) => {
-      root.textContent = (rowData.item.location || rowData.item.address) + ' (' + rowData.item.cap + ')'
-    }
   }
 
 }
