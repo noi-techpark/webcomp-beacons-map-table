2b36bca85c2196ecb873ab12948aa67712e93876
Daniel Rampanelli <hello@danielrampanelli.com>
2019-07-15 10:06:52 +0200

    Improve information displayed by the table component and allow/trigger the details dialog from each entry.

--- packages/beacons-table/src/table.js
+++ packages/beacons-table/src/table.js
@@ -1,5 +1,7 @@
 import { LitElement, html } from 'lit-element'
 import _ from 'lodash'
+import '@polymer/iron-icons/iron-icons.js'
+import '@polymer/paper-icon-button/paper-icon-button.js'
 import '@polymer/paper-spinner/paper-spinner.js'
 import '@vaadin/vaadin-grid/vaadin-grid.js'
 import '@vaadin/vaadin-grid/vaadin-grid-sort-column.js'
@@ -22,14 +24,25 @@ class BeaconsTableView extends LitElement {
           height: 100%;
           width: 100%;
         }
+
+        paper-icon-button {
+          opacity: 0.5;
+          transition: all 0.2s ease-in-out;
+        }
+
+        paper-icon-button:hover {
+          color: #29A8E0;
+          opacity: 1.0;
+        }
       </style>
       <vaadin-grid id="table" theme="row-dividers" multi-sort>
+        <vaadin-grid-column id="detailscolumn" flex-grow="0" width="72px"></vaadin-grid-column>
+        <vaadin-grid-sort-column id="namecolumn" header="NAME" path="name" flex-grow="2"></vaadin-grid-sort-column>
+        <vaadin-grid-sort-column id="placecolumn" header="PLACE" path="place" flex-grow="1"></vaadin-grid-sort-column>
         <vaadin-grid-column id="idcolumn" header="UUID" path="uuid"></vaadin-grid-column>
-        <vaadin-grid-sort-column id="majorcolumn" header="MAJOR" path="major"></vaadin-grid-sort-column>
-        <vaadin-grid-sort-column id="minorcolumn" header="MINOR" path="minor"></vaadin-grid-sort-column>
-        <vaadin-grid-column id="latitudecolumn" header="LATITUDE" path="latitude"></vaadin-grid-column>
-        <vaadin-grid-column id="longitudecolumn" header="LONGITUDE" path="longitude"></vaadin-grid-column>
-        <vaadin-grid-sort-column id="locationcolumn" header="LOCATION" path="composedLocation"></vaadin-grid-sort-column>
+        <vaadin-grid-sort-column id="majorcolumn" header="MAJOR" path="major" flex-grow="0" width="120px"></vaadin-grid-sort-column>
+        <vaadin-grid-sort-column id="minorcolumn" header="MINOR" path="minor" flex-grow="0" width="120px"></vaadin-grid-sort-column>
+        <vaadin-grid-column id="positioncolumn" header="POSITION" path="position"></vaadin-grid-column>
       </vaadin-grid>
     `
   }
@@ -39,7 +52,8 @@ class BeaconsTableView extends LitElement {
 
     self.table.items = beacons.map((beacon) => {
       return _.extend({
-        composedLocation: (beacon.location || beacon.address) + ' (' + beacon.cap + ')'
+        place: (beacon.location || beacon.address) + ', ' + beacon.cap,
+        position: beacon.latitude + ', ' + beacon.longitude
       }, _.cloneDeep(beacon))
     })
   }
@@ -49,8 +63,17 @@ class BeaconsTableView extends LitElement {
     let root = this.shadowRoot
 
     self.table = root.getElementById('table')
-  }
 
+    let detailsColumn = root.getElementById('detailscolumn')
+    detailsColumn.renderer = (root, grid, rowData) => {
+      root.innerHTML = '<paper-icon-button icon="launch" alt="Show the beacon\'s details"></paper-icon-button>'
+      root.querySelector('paper-icon-button').addEventListener('click', () => ((beacon) => {
+        if (!!self.ondetails) {
+          self.ondetails(beacon)
+        }
+      })(rowData.item))
+    }
+  }
 }
 
 customElements.define('beacons-table', BeaconsTableView)
